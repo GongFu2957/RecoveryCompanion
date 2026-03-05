58d2c311a985f54ccfd31e87671c38ff8f64aadc
Derek <accounts@gongfucha.xyz>
2026-03-04 19:56:58 -0600

    260304_1955 Started to Add files for AddEditLogScreen.kt

--- app/src/main/java/com/gongfu/recoverycompanion/logs/presentation/add_edit_logentry/AddEditLogScreen.kt
+++ app/src/main/java/com/gongfu/recoverycompanion/logs/presentation/add_edit_logentry/AddEditLogScreen.kt
@@ -2,7 +2,6 @@ package com.gongfu.recoverycompanion.logs.presentation.add_edit_logentry
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun AddEditLogScreen(
